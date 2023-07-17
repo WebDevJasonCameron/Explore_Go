5a4bf22d835b918af1be0329d218915fd2c0f005
Jason Cameron <dev.jasoncameron@gmail.com>
2023-07-17 05:54:52 -0500

    switch ex comp

--- Udemy/ztmWorkbookOne/src/lectures/exercise/switch/switch.go
+++ Udemy/ztmWorkbookOne/src/lectures/exercise/switch/switch.go
@@ -14,4 +14,19 @@ package main
 import "fmt"
 
 func main() {
+
+	switch age := 43; {
+	case age == 0:
+		fmt.Println("newborn")
+	case age <= 3:
+		fmt.Println("toddler")
+	case age <= 12:
+		fmt.Println("child")
+	case age <= 17:
+		fmt.Println("teenager")
+	case age >= 18:
+		fmt.Println("adult")
+	default:
+		fmt.Println("No age provided")
+	}
 }
