3e39c90af906d5b34949dcf1ff8fe46f3266423e
Jason Cameron <dev.jasoncameron@gmail.com>
2023-07-22 06:39:02 -0500

    range demo comp

--- Udemy/ztmWorkbookOne/src/lectures/demo/ranges/ranges.go
+++ Udemy/ztmWorkbookOne/src/lectures/demo/ranges/ranges.go
@@ -3,4 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
+	slice := []string{"Hello", "world", "!"}
+
+	for i, element := range slice { //   Range will return index and element
+		fmt.Println(i, element, ":")
+
+		for _, ch := range element { //   Will print out each letter (%q prints rune, not just a bit)
+			fmt.Printf(" %q\n", ch)
+		}
+	}
+
 }
