185e4581641a699e29cd751b71575ca8bf7b790c
Jason Cameron <dev.jasoncameron@gmail.com>
2023-07-24 06:20:26 -0500

    work on pointers in demo

--- Udemy/ztmWorkbookOne/src/lectures/demo/pointers/pointers.go
+++ Udemy/ztmWorkbookOne/src/lectures/demo/pointers/pointers.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type Counter struct {
+	hits int
+}
+
+func increment(counter *Counter) {
+	counter.hits += 1
+	fmt.Println("Counter", counter)
+}
+
+func replace(old *string, new string, counter *Counter) {
+	*old = new
+	increment(counter)
+}
+
 func main() {
+
+	counter := Counter{}
+
+	greet := "Hello"
+	world := "World"
+
+	fmt.Println(greet, world)
+
+	replace(&greet, "Hi", &counter)
+	fmt.Println(greet, world)
 
 }
