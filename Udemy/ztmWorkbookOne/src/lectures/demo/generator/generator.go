a55315932941298a13f2d17b47b2304ac88adb54
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-07 17:03:02 -0500

    generator demo compl

--- Udemy/ztmWorkbookOne/src/lectures/demo/generator/generator.go
+++ Udemy/ztmWorkbookOne/src/lectures/demo/generator/generator.go
@@ -1,4 +1,68 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandInt(min, max int) <-chan int {
+	out := make(chan int, 3)
+
+	go func() {
+		for {
+			out <- rand.Intn(max-min+1) + min
+		}
+	}()
+	return out
+}
+
+func generateRandIntn(count, min, max int) <-chan int {
+
+	out := make(chan int, 1)
+
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Intn(max-min+1) + min
+		}
+		close(out)
+	}()
+
+	return out
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	randInt := generateRandInt(1, 100)
+
+	fmt.Println("GenerateRandInt infinite")
+
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+
+	fmt.Println("GenerateRandInt using finite count")
+
+	randIntnRange := generateRandIntn(2, 1, 10)
+
+	for i := range randIntnRange {
+		fmt.Println(i)
+	}
+
+	fmt.Println("GenerateRandInt using finite count and determine if open")
+
+	randIntn := generateRandIntn(3, 1, 10)
+
+	for {
+		n, open := <-randIntn
+		if !open {
+			if !open {
+				break
+			}
+			fmt.Println(n)
+		}
+	}
+
 }
