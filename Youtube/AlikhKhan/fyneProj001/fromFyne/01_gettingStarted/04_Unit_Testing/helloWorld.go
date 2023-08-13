cd6def8689d703019a916ea198482622c425a79c
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-13 16:54:46 -0500

    build a test in fyne

---
+++ Youtube/AlikhKhan/fyneProj001/fromFyne/01_gettingStarted/04_Unit_Testing/helloWorld.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func makeUI() (*widget.Label, *widget.Entry) {
+	out := widget.NewLabel("Hello World!")
+	in := widget.NewEntry()
+
+	in.OnChanged = func(content string) {
+		out.SetText("Hello " + content + "!")
+	}
+
+	return out, in
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello Person")
+
+	w.SetContent(container.NewVBox(makeUI()))
+	w.ShowAndRun()
+}
