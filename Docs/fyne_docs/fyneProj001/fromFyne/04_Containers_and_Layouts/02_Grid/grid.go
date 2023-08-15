6317689d94c17bc3049297980736bdd6e24cab49
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-15 18:38:43 -0500

    grid layout

--- Docs/fyne_docs/fyneProj001/fromFyne/04_Containers_and_Layouts/02_Grid/grid.go
+++ Docs/fyne_docs/fyneProj001/fromFyne/04_Containers_and_Layouts/02_Grid/grid.go
@@ -17,7 +17,12 @@ func main() {
 	t1 := canvas.NewText("1", color.White)
 	t2 := canvas.NewText("2", color.White)
 	t3 := canvas.NewText("3", color.White)
-	grid := container.New(layout.NewGridLayout(2), t1, t2, t3)
+	t4 := canvas.NewText("4", color.White)
+	t5 := canvas.NewText("5", color.White)
+	t6 := canvas.NewText("6", color.White)
+	t7 := canvas.NewText("7", color.White)
+	// grid := container.New(layout.NewGridLayout(3), t1, t2, t3, t4, t5, t6, t7)
+	grid := container.New(layout.NewGridWrapLayout(fyne.NewSize(50, 50)), t1, t2, t3, t4, t5, t6, t7)
 
 	w.SetContent(grid)
 	w.Resize(fyne.NewSize(150, 150))
