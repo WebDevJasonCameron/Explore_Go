5fd86bcc6bd1218f18fe1dfd619d23548c4cf8ab
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-15 18:46:44 -0500

    border

---
+++ Docs/fyne_docs/fyneProj001/fromFyne/04_Containers_and_Layouts/03_Border/border.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Border Layout")
+
+	top := canvas.NewText("top bar", color.White)
+	left := canvas.NewText("left", color.White)
+	middle := canvas.NewText("content", color.White)
+	content := container.NewBorder(top, nil, left, nil, middle)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(200, 200))
+	w.ShowAndRun()
+}
