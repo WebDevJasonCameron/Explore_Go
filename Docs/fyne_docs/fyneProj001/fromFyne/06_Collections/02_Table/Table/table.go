d7b3ae0a42b31ecc44d119e876b81bd78a73ad05
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-18 06:18:31 -0500

    table collections

---
+++ Docs/fyne_docs/fyneProj001/fromFyne/06_Collections/02_Table/Table/table.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+var data = [][]string{
+	[]string{"top left", "top right"},
+	[]string{"bottom left", "bottom right"}}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Table Widget")
+
+	list := widget.NewTable(
+		func() (int, int) {
+			return len(data), len(data[0])
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("wide content")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i.Row][i.Col])
+		},
+	)
+
+	w.SetContent(list)
+	w.Resize(fyne.NewSize(400, 400))
+	w.ShowAndRun()
+
+}
