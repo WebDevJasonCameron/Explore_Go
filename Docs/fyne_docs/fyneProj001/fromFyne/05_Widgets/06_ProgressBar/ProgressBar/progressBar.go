9841a85e1c054dbf63a908db61c46510c17ccf60
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-17 19:56:59 -0500

    Progress Bar Widget

---
+++ Docs/fyne_docs/fyneProj001/fromFyne/05_Widgets/06_ProgressBar/ProgressBar/progressBar.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("ProgressBar Widget")
+
+	progress := widget.NewProgressBar()
+	infinite := widget.NewProgressBarInfinite()
+
+	go func() {
+		for i := 0.0; i <= 1.0; i += 0.1 {
+			time.Sleep(time.Millisecond * 250)
+			progress.SetValue(i)
+		}
+	}()
+
+	w.SetContent(container.NewVBox(progress, infinite))
+	w.Resize(fyne.NewSize(250, 250))
+	w.ShowAndRun()
+}
