9e2dd299dbbb7b31f0fc94e00495807984b3be2f
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-19 10:38:05 -0500

    tree collection

---
+++ Docs/fyne_docs/fyneProj001/fromFyne/06_Collections/03_Tree/Tree/tree.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Table Widget")
+
+	tree := widget.NewTree(
+		func(id widget.TreeNodeID) []widget.TreeNodeID {
+
+			switch id {
+			case "":
+				return []widget.TreeNodeID{"a", "b", "c"}
+			case "a":
+				return []widget.TreeNodeID{"a1", "a2"}
+			}
+			return []string{}
+		},
+		func(id widget.TreeNodeID) bool {
+			return id == "" || id == "a"
+		},
+		func(branch bool) fyne.CanvasObject {
+			if branch {
+				return widget.NewLabel("Branch template")
+			}
+			return widget.NewLabel("Leaf template")
+		},
+		func(id widget.TreeNodeID, branch bool, o fyne.CanvasObject) {
+
+			text := id
+
+			if branch {
+				text += " (branch)"
+			}
+
+			o.(*widget.Label).SetText(text)
+		},
+	)
+
+	w.SetContent(tree)
+	w.Resize(fyne.NewSize(250, 250))
+	w.ShowAndRun()
+}
