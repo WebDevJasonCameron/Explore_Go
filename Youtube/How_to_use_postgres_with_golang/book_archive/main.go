e178c86ae52d680a884471672e6fa09e055c3b2f
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-21 18:33:45 -0500

    testing a way to push out hidden env modulars without using third party

--- Youtube/How_to_use_postgres_with_golang/book_archive/main.go
+++ Youtube/How_to_use_postgres_with_golang/book_archive/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	// "fmt"
+	// "os"
+
+	"fmt"
+
+	"gorm.io/gorm"
+	//"gorm.io/driver/sqlite"
+)
+
+type Person struct {
+	gorm.Model
+
+	Name  string
+	Email string `gorm:"typevarchar(100);unique_index"`
+	Books []Book
+}
+
+type Book struct {
+	gorm.Model
+
+	Title      string
+	Author     string
+	CallNumber string `gorm:"unique_index"`
+	PersonID   string
+}
+
+var db *gorm.DB
+var err error
+
+func main() {
+
+	greeting := modularTest()
+
+	fmt.Println(greeting)
+
+	// Loading evn vars
+	// dialect := os.Getenv(DIALECT)
+	// host := os.Getenv(HOST)
+	// dbPort := os.Getenv(DBPORT)
+	// user := os.Getenv(USER)
+	// dbName := os.Getenv(NAME)
+	// password := os.Getenv(PASSWORD)
+
+	// DB connection string
+	// dbURI := fmt.Sprintf("host=%s user=%s ssllmode=disable password%s port=%s", host, user, password, dbPort)
+}
