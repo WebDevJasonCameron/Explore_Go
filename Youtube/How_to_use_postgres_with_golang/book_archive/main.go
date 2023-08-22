8a2843643026f84b8504014c55fab37d64a55003
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-22 05:28:27 -0500

    piping in env with hidden go package

--- Youtube/How_to_use_postgres_with_golang/book_archive/main.go
+++ Youtube/How_to_use_postgres_with_golang/book_archive/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	DbEnv "book_archive/env"
 	"fmt"
+	"os"
 
-	DbENV "book_archive/env"
+	"gorm.io/gorm"
+	//"gorm.io/driver/sqlite"
 )
 
+// STRUCTs
+type Person struct {
+	gorm.Model
+
+	Name  string
+	Email string `gorm:"typevarchar(100);unique_index`
+	Books []Book
+}
+
+type Book struct {
+	gorm.Model
+
+	Title      string
+	Author     string
+	CallNumber string `gorm:"unique_index`
+	PersonID   int
+}
+
+// VARs
+var db *gorm.DB
+var err error
+
 func main() {
-	fmt.Println(DbENV.DbNAME)
-	fmt.Println(DbENV.DbHOST)
-	fmt.Println(DbENV.DbNAME)
-	fmt.Println(DbENV.DbPASSWORD)
-	fmt.Println(DbENV.DbPORT)
-	fmt.Println(DbENV.DbUSER)
+
+	// Loading ENV VARs
+	dbDialect := os.Getenv(DbEnv.DbDIALECT)
+	dbHost := os.Getenv(DbEnv.DbHOST)
+	dbPort := os.Getenv(DbEnv.DbPORT)
+	dbUser := os.Getenv(DbEnv.DbUSER)
+	dbName := os.Getenv(DbEnv.DbNAME)
+	dbPassword := os.Getenv(DbEnv.DbPASSWORD)
+
+	// DB Connection String
+	dbUri := fmt.Sprintf("host=%s user%s dbname%s sslmode=disable password=%s port=%s", dbHost, dbUser, dbName, dbPassword, dbPort)
+
+	// Openning Connection to DB
+	db, err = gorm.Open(dbDialect, dbUri)
+
 }
