edc48099710432aed8fa5a448b29a1abc817bff5
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-21 20:28:35 -0500

    test env.go

--- Youtube/How_to_use_postgres_with_golang/book_archive/main.go
+++ Youtube/How_to_use_postgres_with_golang/book_archive/main.go
@@ -1,49 +1,16 @@
 package main
 
 import (
-	// "fmt"
-	// "os"
-
 	"fmt"
 
-	"gorm.io/gorm"
-	//"gorm.io/driver/sqlite"
+	DbENV "book_archive/env"
 )
 
-type Person struct {
-	gorm.Model
-
-	Name  string
-	Email string `gorm:"typevarchar(100);unique_index"`
-	Books []Book
-}
-
-type Book struct {
-	gorm.Model
-
-	Title      string
-	Author     string
-	CallNumber string `gorm:"unique_index"`
-	PersonID   string
-}
-
-var db *gorm.DB
-var err error
-
 func main() {
-
-	greeting := modularTest()
-
-	fmt.Println(greeting)
-
-	// Loading evn vars
-	// dialect := os.Getenv(DIALECT)
-	// host := os.Getenv(HOST)
-	// dbPort := os.Getenv(DBPORT)
-	// user := os.Getenv(USER)
-	// dbName := os.Getenv(NAME)
-	// password := os.Getenv(PASSWORD)
-
-	// DB connection string
-	// dbURI := fmt.Sprintf("host=%s user=%s ssllmode=disable password%s port=%s", host, user, password, dbPort)
+	fmt.Println(DbENV.DbNAME)
+	fmt.Println(DbENV.DbHOST)
+	fmt.Println(DbENV.DbNAME)
+	fmt.Println(DbENV.DbPASSWORD)
+	fmt.Println(DbENV.DbPORT)
+	fmt.Println(DbENV.DbUSER)
 }
