577abe3877554ef3d475c8e4289449addad35621
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-23 05:35:21 -0500

    bending towards sqlx only.  i think it might not work

--- Youtube/How_to_use_postgres_with_golang/book_archive/main.go
+++ Youtube/How_to_use_postgres_with_golang/book_archive/main.go
@@ -2,49 +2,38 @@ package main
 
 import (
 	DbEnv "book_archive/env"
-	"fmt"
 	"os"
 
-	"gorm.io/gorm"
-	//"gorm.io/driver/sqlite"
+	"github.com/jmoiron/sqlx"
 )
 
 // STRUCTs
 type Person struct {
-	gorm.Model
-
 	Name  string
 	Email string `gorm:"typevarchar(100);unique_index`
 	Books []Book
 }
 
 type Book struct {
-	gorm.Model
-
 	Title      string
 	Author     string
 	CallNumber string `gorm:"unique_index`
 	PersonID   int
 }
 
-// VARs
-var db *gorm.DB
-var err error
-
 func main() {
 
 	// Loading ENV VARs
-	dbDialect := os.Getenv(DbEnv.DbDIALECT)
-	dbHost := os.Getenv(DbEnv.DbHOST)
-	dbPort := os.Getenv(DbEnv.DbPORT)
-	dbUser := os.Getenv(DbEnv.DbUSER)
-	dbName := os.Getenv(DbEnv.DbNAME)
+	//dbDialect := os.Getenv(DbEnv.DbDIALECT)
+	//dbHost := os.Getenv(DbEnv.DbHOST)
+	//dbPort := os.Getenv(DbEnv.DbPORT)
+	dbUser := "user=" + DbEnv.DbUSER
+	dbName := "name=" + DbEnv.DbNAME
 	dbPassword := os.Getenv(DbEnv.DbPASSWORD)
 
-	// DB Connection String
-	dbUri := fmt.Sprintf("host=%s user%s dbname%s sslmode=disable password=%s port=%s", dbHost, dbUser, dbName, dbPassword, dbPort)
+	dbString := dbUser + " " + dbName + " " + "sslmode=disable"
 
 	// Openning Connection to DB
-	db, err = gorm.Open(dbDialect, dbUri)
+	db, err := sqlx.Connect("postgres", dbString)
 
 }
